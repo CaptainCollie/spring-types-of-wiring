e362575592e3b105de3b475b30ee89817c0b93af
CaptainCollie <shadaraevd@gmail.com>
2020-12-28 23:41:14 +0800

    new

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,5 +1,6 @@
 package koschei;
 
+import koschei.models.Needle7;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,8 @@ public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
 
+    private Needle7 needle;
+
     public String getRulesByDeth() {
         return "На свете есть океан , " + ocean.toString();
     }
@@ -16,5 +19,10 @@ public class KoscheiTheDeathless {
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
+    }
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
     }
 }
